71413e004d5d58747017ff66e7a859b56e0b6b3d
Peter SZTANOJEV <sztanpet@gmail.com>
2019-06-10 19:01:47 +0200

    add display-test command

---
+++ cmd/display-test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"code.sztanpet.net/barcode-scanner/internal/display"
+)
+
+func main() {
+	// TODO burn-in!
+	// TODO led
+	s, err := display.NewScreen()
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return
+	}
+
+	// 4 sor max, a nulladik lesz a tetejen
+	_ = s.WriteLine(0, "NULLADIK")
+	_ = s.WriteLine(1, "HULLO")
+	_ = s.WriteLine(2, "BUFF")
+	_ = s.WriteLine(3, "DIKKDIKKDIKK")
+
+	<-time.After(5 * time.Second)
+	_ = s.Blank()
+}
