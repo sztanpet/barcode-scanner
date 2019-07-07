bdc88b29ffda536583b14a19bc25f5b699ad5281
Peter SZTANOJEV <sztanpet@gmail.com>
2019-07-07 20:56:21 +0200

    display - implement basics of the UI: displaying title line, help line, fix locking

--- go.mod
+++ go.mod
@@ -5,10 +5,11 @@ go 1.12
 require (
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/juju/loggo v0.0.0-20190526231331-6e530bcce5d8
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
-	golang.org/x/image v0.0.0-20190523035834-f03afa92d3ff
+	golang.org/x/image v0.0.0-20190703141733-d6a02ce849c9
 	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 	google.golang.org/appengine v1.6.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
--- internal/display/display.go
+++ internal/display/display.go
@@ -3,11 +3,15 @@ package display
 import (
 	"context"
 	"image"
+	"image/color"
+	"image/draw"
 	"sync"
 	"time"
 
+	"github.com/golang/freetype/truetype"
 	"github.com/juju/loggo"
 	"golang.org/x/image/font"
+	"golang.org/x/image/font/gofont/gosmallcaps"
 	"golang.org/x/image/font/inconsolata"
 	"golang.org/x/image/math/fixed"
 	"periph.io/x/periph/conn/i2c/i2creg"
@@ -16,25 +20,41 @@ import (
 	"periph.io/x/periph/host"
 )
 
+var logger = loggo.GetLogger("main.display")
+
+// The ScreenTimeout duration after which the display is blanked to prevent burn-in.
+var ScreenTimeout = 10 * time.Minute
+
 // lineCount defines how many lines of text fit on the screen
 const lineCount = 4
 
-// mediumFont is the font used for displaying default text
 var mediumFont = inconsolata.Bold8x16
+var titleFont = inconsolata.Regular8x16
+var helpFont font.Face
 
-// The ScreenTimeout after which the display is blanked to prevent burn-in.
-var ScreenTimeout = 10 * time.Minute
-
-var logger = loggo.GetLogger("main.display")
+const white = image1bit.On
+const black = image1bit.Off
 
 type Screen struct {
 	ctx context.Context
 	dev *ssd1306.Dev
-	img *image1bit.VerticalLSB
 
 	mu         sync.Mutex
+	img        *image1bit.VerticalLSB
 	lines      []string
 	lastActive time.Time
+}
+
+func init() {
+	// tiny help text font on the bottom
+	ff, err := truetype.Parse(gosmallcaps.TTF)
+	if err != nil {
+		panic(err.Error())
+	}
+	helpFont = truetype.NewFace(ff, &truetype.Options{
+		Size:    12,
+		Hinting: font.HintingNone,
+	})
 }
 
 func NewScreen(ctx context.Context) (*Screen, error) {
@@ -69,6 +89,7 @@ func NewScreen(ctx context.Context) (*Screen, error) {
 	}, nil
 }
 
+// Lines returns the currently displayed lines of text on the screen
 func (s *Screen) Lines() []string {
 	s.mu.Lock()
 	defer s.mu.Unlock()
@@ -79,50 +100,117 @@ func (s *Screen) Lines() []string {
 	return ret
 }
 
-func (s *Screen) WriteLine(linenum int, text string) error {
-	s.MarkActivity()
-	s.mu.Lock()
-	s.lines[linenum] = text
-	s.mu.Unlock()
-
-	height := s.img.Bounds().Dy() - mediumFont.Descent
+func (s *Screen) writeUnlocked(f font.Face, line int, c color.Color, centered bool, text string) {
+	m := f.Metrics()
+	height := s.img.Bounds().Dy() - m.Descent.Round()
 	// by default, 0th line is at the bottom, 3rd is at the top,
 	// invert it, because it feels better
 	// 0th line should be the top, 3rd line should be at the bottom
-	height -= (3 - linenum) * mediumFont.Height
+	height -= (3 - line) * m.Height.Round()
 	drawer := font.Drawer{
 		Dst:  s.img,
-		Src:  &image.Uniform{image1bit.On},
-		Face: mediumFont,
+		Src:  &image.Uniform{c},
+		Face: f,
 		Dot:  fixed.P(0, height),
 	}
+	bounds, adv := drawer.BoundString(text)
+
+	// adjust text start position
+	if centered {
+		x := s.img.Bounds().Dx()/2 - adv.Round()/2
+		drawer.Dot = fixed.P(x, height)
+	}
+
+	// if we need to write in black, the background needs to be white,
+	bg := black
+	if c == black {
+		bg = white
+	}
+
+	// add 2 pixels to the height because it looks better that way
+	r := image.Rect(bounds.Min.X.Round(), bounds.Min.Y.Round(), s.img.Bounds().Dx(), height+2)
+	draw.Draw(s.img, r, &image.Uniform{bg}, image.ZP, draw.Src)
 
 	drawer.DrawString(text)
+}
+
+// WriteTitle draws the text in black on a white background into the first line (line #0)
+func (s *Screen) WriteTitle(text string) error {
+	s.MarkActivity()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.lines[0] = text
+
+	s.writeUnlocked(titleFont, 0, black, true, text)
 	return s.Draw()
 }
 
+// WriteLine writes the text in white on black into the indicated line (usually #1 or #2)
+func (s *Screen) WriteLine(line int, text string) error {
+	s.MarkActivity()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.lines[line] = text
+
+	s.writeUnlocked(mediumFont, line, white, false, text)
+	return s.drawUnlocked()
+}
+
+// WriteHelp writes help text in black on white into the last line (line #3)
+func (s *Screen) WriteHelp(text string) error {
+	s.MarkActivity()
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.lines[lineCount-1] = text
+	s.writeUnlocked(helpFont, lineCount-1, black, true, text)
+	return s.drawUnlocked()
+}
+
+// Clear clears the image
+func (s *Screen) Clear() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	s.img = image1bit.NewVerticalLSB(s.dev.Bounds())
+	return s.drawUnlocked()
+}
+
+// Draw display the image
 func (s *Screen) Draw() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	return s.drawUnlocked()
+}
+
+func (s *Screen) drawUnlocked() error {
 	return s.dev.Draw(s.dev.Bounds(), s.img, image.Point{})
 }
 
+// Blank blanks the screen without clearing the image
 func (s *Screen) Blank() error {
 	s.MarkActivity()
 	return s.dev.Halt()
 }
 
+// MarkActivity explicitly cancels the screen-saver (most anything else implicitly does it)
 func (s *Screen) MarkActivity() {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
 	s.lastActive = time.Now()
 }
 
 func (s *Screen) shouldBlank() bool {
 	s.mu.Lock()
 	defer s.mu.Unlock()
+
 	blankAfter := s.lastActive.Add(ScreenTimeout)
 	return time.Now().After(blankAfter)
 }
 
+// HandleScreenSaver blanks the screen after ScreenTimeout idle duration
 func (s *Screen) HandleScreenSaver() {
 	t := time.NewTicker(1 * time.Minute)
 	for {
